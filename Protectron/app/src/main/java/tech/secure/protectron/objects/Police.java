cf1a772718685c882590412550a0a4ef9b4d3ba5
mac craig <maccraig98@gmail.com>
2017-11-29 10:10:50 -0800

    Create emergency activity

---
+++ Protectron/app/src/main/java/tech/secure/protectron/objects/Police.java
@@ -0,0 +1,25 @@
+package tech.secure.protectron.objects;
+
+/**
+ * Created by maccraig on 2017-11-28.
+ */
+
+public class Police {
+    private String STRNUM;
+    private String STRNAM;
+    private String BLDGNAM;
+
+    public String getNum() {
+        return STRNUM;
+    }
+
+    public String getName() {
+        return STRNAM;
+    }
+
+    public String getBuilding() {
+        return BLDGNAM;
+    }
+}
+
+
