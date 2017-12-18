5051faca237bc3f7fbffa1b8b689073de933c531
Morgan Ariss <morgan.ariss.93@gmail.com>
2017-12-18 13:43:08 -0800

    Initial commit for the new app, initial structure present, plus login and registration pages.

---
+++ TheBlackList/app/src/main/java/suplex/theblacklist/Landing.java
@@ -0,0 +1,15 @@
+package suplex.theblacklist;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Landing extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.landing);
+
+        //splash screen before login
+    }
+}
